011ee16df2a91c13dd036a70835dfd03d381bd81
brianlu90 <brianlu90@gmail.com>
2021-02-24 05:49:39 -0800

    add helloworld.cpp

---
+++ helloworld.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main (void)
+{
+    int i;
+
+    for (i = 0; i < 10; i++) {
+        cout << "hello world " << i << " times!" << endl;
+    }
+    return 0;
+}
